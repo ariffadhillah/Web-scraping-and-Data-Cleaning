e69d538edf8158a462ef2ae0ca4c5d39f58dd8cc
ariffadhillah <ariffadhillah319@gmail.com>
2023-05-05 09:03:36 +0700

    add discription product imported

--- page-product-endless.py
+++ page-product-endless.py
@@ -2,35 +2,43 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://www.endless-sport.co.jp/products/brake_pad/carlist/honda/HONDA_Accord.html"
+url = "https://www.endless-sport.co.jp/products/brake_pad/BrakePad_Carlist/import/BrakePad_ALFAROMEO.html"
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58'
 }
-fields = ['Parttype (category)', 'Parttype url (category)', 'Make', 'Make URL', 'Model', 'Year', 'Engine cc', 'Type', 'PartNumber']
-filename = 'endless.csv'
+
+fields = ['Parttype (category)', 'Parttype URL (category)', 'Make', 'Make URL', 'Model', 'Model URL', 'Year', 'Engine cc', 'Type', 'PartNumber']
+filename = 'Example.csv'
 
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.content, 'lxml')
 data = []
 
-parttypeCategory = soup.find('div', id='caution_contents')
-parttypeCategorylinks = parttypeCategory.find_all('a')
-category = parttypeCategorylinks[1]
-parttype_category_url = category['href']
-parttype_category = category.text.strip()
+cautionContents = soup.find('div', {'class': 'caution_contents'}) or soup.find('div', {'id': 'caution_contents'})
+if cautionContents:
+    caution_links = cautionContents.find_all('a')
+    category = caution_links[1]
+    parttype_category_url = category['href']
+    parttype_category = category.text.strip()
 
 # menhapus <tr> dengan ketinggian tertentu seperti contoh ini tinggi 'height: 149px'
-iframes = soup.find_all('iframe')
-for iframe in iframes:
-    src = iframe['src']
-    iframe_content = requests.get(src).content
-    iframe_soup = BeautifulSoup(iframe_content, 'html.parser')
-    tables = iframe_soup.find_all('table')
-    for tablemodel in tables:
-        rowsmodel = tablemodel.find_all('tr')[1]
-        model =  rowsmodel.find_all('td')
+iframesimport = soup.find_all('iframe')
+for iframeimport in iframesimport:
+    src = iframeimport['src']
+    iframeimport_content = requests.get(src).content
+    iframeimport_soup = BeautifulSoup(iframeimport_content, 'html.parser')
+    tablesimport = iframeimport_soup.find_all('table')
+    # print(tables)
+    for tableMake in tablesimport:
+        rowsMake = tableMake.find_all('tr')[1]
+        makeimport =  rowsMake.find_all('td')[1]
+    
+    for tablemodelimport in tablesimport:
+        rowsmodelimport = tablemodelimport.find_all('tr')[2]
+        modelimport =  rowsmodelimport.find_all('td')[1]
+        
 
-    for table in tables:
+    for table in tablesimport:
         rows = table.find_all('tr')[5:]
         for row in rows:
             # tambahkan kondisi untuk memeriksa apakah elemen tr memiliki atribut style yang mengatur tinggi
@@ -40,27 +48,95 @@ for iframe in iframes:
             engine = row.find_all('td')
             type_ = row.find_all('td')
             PartNumberFront = row.find_all('td')
-            PartNumberRear = row.find_all('td')[13:]
+            PartNumberRear = row.find_all('td')[10:]
             if PartNumberFront and PartNumberRear:
-                PartNumber = PartNumberFront[4].text.strip() +' '+ PartNumberRear[0].text.strip()
+                PartNumber = PartNumberFront[5].text.strip() +' '+ PartNumberRear[0].text.strip()
             else:
                 PartNumber = ""
             
             if year and engine and type_ and PartNumber:
                 endless = {
                     'Parttype (category)': parttype_category,
-                    'Parttype url (category)': parttype_category_url,
-                    'Make': url.split('/')[-2].replace('-', ' ').title(),
-                    'Make URL': url,
-                    'Model': model[1].text.strip(),
-                    'Year': year[1].text.strip().replace('～', ' ～ '),
-                    'Engine cc': engine[2].text.strip().replace('～', ' ～ '),
-                    'Type': type_[3].text.strip(),
+                    'Parttype URL (category)': parttype_category_url,
+                    'Make': makeimport.text.strip(),
+                    'Make URL': '',
+                    'Model': modelimport.text.strip(),
+                    'Model URL': '',
+                    'Year': year[3].text.strip().replace('～', ' ～ '),
+                    'Engine cc': ' ',
+                    'Type': type_[4].text.strip(),
                     'PartNumber': PartNumber.replace(' ', '\n'),
                 }
                 data.append(endless)
-                print('Saving',endless['Parttype (category)'],endless['Parttype url (category)'],endless['Make'],endless['Make URL'],endless['Model'], endless['Year'], endless['Engine cc'], endless['Type'], endless['PartNumber'])
+                print('Saving',endless['Parttype (category)'], endless['Parttype URL (category)'], endless['Make'], endless['Make URL'], endless['Model'], endless['Model URL'], endless['Year'], endless['Engine cc'], endless['Type'], endless['PartNumber'])
+                
+                with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=fields)
+                    writer.writeheader()
+                    for item in data:
+                        writer.writerow(item)
 
+
+hreflink = "https://www.endless-sport.co.jp/products/brake_pad/BrakePad_Carlist/import/BrakePad_VOLVO.html"
+r = requests.get(hreflink, headers=headers)
+soup = BeautifulSoup(r.content, 'lxml')
+
+cautionContents = soup.find('div', {'class': 'caution_contents'}) or soup.find('div', {'id': 'caution_contents'})
+if cautionContents:
+    caution_links = cautionContents.find_all('a')
+    category = caution_links[1]
+    parttype_category_url = category['href']
+    parttype_category = category.text.strip()
+
+# menhapus <tr> dengan ketinggian tertentu seperti contoh ini tinggi 'height: 149px'
+iframesimport = soup.find_all('iframe')
+for iframeimport in iframesimport:
+    src = iframeimport['src']
+    iframeimport_content = requests.get(src).content
+    iframeimport_soup = BeautifulSoup(iframeimport_content, 'html.parser')
+    tablesimport = iframeimport_soup.find_all('table')
+    # print(tables)
+    for tableMake in tablesimport:
+        rowsMake = tableMake.find_all('tr')[1]
+        makeimport =  rowsMake.find_all('td')[1]
+    
+    for tablemodelimport in tablesimport:
+        rowsmodelimport = tablemodelimport.find_all('tr')[2]
+        modelimport =  rowsmodelimport.find_all('td')[1]
+        
+
+    for table in tablesimport:
+        rows = table.find_all('tr')[5:]
+        for row in rows:
+            # tambahkan kondisi untuk memeriksa apakah elemen tr memiliki atribut style yang mengatur tinggi
+            if row.has_attr('style') and 'height: 149px' in row['style'] and 'px' in row['style']:
+                continue  # lewati jika ada atribut style dengan ketinggian yang ditentukan
+            year = row.find_all('td')
+            engine = row.find_all('td')
+            type_ = row.find_all('td')
+            PartNumberFront = row.find_all('td')
+            PartNumberRear = row.find_all('td')[10:]
+            if PartNumberFront and PartNumberRear:
+                PartNumber = PartNumberFront[5].text.strip() +' '+ PartNumberRear[0].text.strip()
+            else:
+                PartNumber = ""
+            
+            if year and engine and type_ and PartNumber:
+                endless = {
+                    'Parttype (category)': parttype_category,
+                    'Parttype URL (category)': parttype_category_url,
+                    'Make': makeimport.text.strip(),
+                    'Make URL': '',
+                    'Model': modelimport.text.strip(),
+                    'Model URL': '',
+                    'Year': year[3].text.strip().replace('～', ' ～ '),
+                    'Engine cc': ' ',
+                    'Type': type_[4].text.strip(),
+                    'PartNumber': PartNumber.replace(' ', '\n'),
+                }
+                data.append(endless)
+                print('Saving',endless['Parttype (category)'], endless['Parttype URL (category)'], endless['Make'], endless['Make URL'], endless['Model'], endless['Model URL'], endless['Year'], endless['Engine cc'], endless['Type'], endless['PartNumber'])
+                
                 with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
                     writer = csv.DictWriter(csvfile, fieldnames=fields)
                     writer.writeheader()
--- test_endless.py
+++ test_endless.py
@@ -1,81 +1,165 @@
+# import requests
+# from bs4 import BeautifulSoup
+# import csv
+# import time
+
+# # Set the base URL and user agent
+# baseurl = 'https://www.endless-sport.co.jp'
+# headers = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58'
+# }
+
+# # Create a set to store processed URLs to avoid duplicates
+# processed_urls = set()
+
+# # Create a list to store category product names
+# categoryProduct = []
+
+# # Set the URL for brake pads category and get the page content
+# pageBrakepads = baseurl + '/products/brake_pad/car_list.html'
+# r = requests.get(pageBrakepads, headers=headers)
+# soup = BeautifulSoup(r.content, 'lxml')
+
+# # Find all the car names on the brake pads category page
+# listCarsName = []
+# brakepad_search_whole = soup.find('div', id='brakepad_search_whole')
+# carList = brakepad_search_whole.find('div', id='right_menu_box')
+# for linkcars in carList.find_all('a', href=True):
+#     listCarsName.append(linkcars['href'])
+
+# # Set the field names for the CSV file
+# fields = ['Year', 'Engine cc', 'Type']
+# filename = 'contoh.csv'
+
+# # Create a list to store all product URLs for all car models
+# productList = []
+
+# # Loop through all car models and get their product URLs
+# for carName in listCarsName:
+#     urlcar = requests.get(carName, headers=headers)
+#     soup = BeautifulSoup(urlcar.content, 'lxml')
+#     carNameIcons = soup.find_all('div', id='ewig_pad_whole')
+#     for itemCarName in carNameIcons:
+#         for href in itemCarName.find_all('a',  href=True):
+#             # Append product URL to the list only if it hasn't been processed before
+#             url = href['href']
+#             if url not in processed_urls:
+#                 productList.append(baseurl + url)
+#                 processed_urls.add(url)
+#             time.sleep(.5)
+            
+# # Loop through all product URLs and save the product data to CSV file
+# with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+#     writer = csv.DictWriter(csvfile, fieldnames=fields)
+#     writer.writeheader()
+#     for pageCarlist in productList:
+#         r = requests.get(pageCarlist, headers=headers)
+#         soup = BeautifulSoup(r.content, 'lxml')
+#         time.sleep(.1)
+#         data = []
+#         iframes = soup.find_all('iframe')
+#         for iframe in iframes:
+#             src = iframe['src']
+#             iframe_content = requests.get(src).content
+#             iframe_soup = BeautifulSoup(iframe_content, 'html.parser')
+#             tables = iframe_soup.find_all('table')
+#             for table in tables:
+#                 rows = table.find_all('tr')[5:]
+#                 for row in rows:
+#                     if row.has_attr('style') and 'height: 149px' in row['style'] and 'px' in row['style']:
+#                         continue
+#                     year = row.find_all('td')[1].text.strip().replace('～', ' ～ ')
+#                     engine = row.find_all('td')[2].text.strip()
+#                     type_ = row.find_all('td')[3].text.strip()
+#                     endless = {
+#                         'Year': year,
+#                         'Engine cc': engine,
+#                         'Type': type_,
+#                     }
+#                     data.append(endless)
+#                     print('Saving',endless['Make'], endless['Year'], endless['Engine cc'], endless['Type'], endless['PartNumber'])
+
+
+
 import requests
 from bs4 import BeautifulSoup
 import csv
-import time
 
-# Set the base URL and user agent
-baseurl = 'https://www.endless-sport.co.jp'
+# first URL
+url1 = "https://www.endless-sport.co.jp/products/brake_pad/BrakePad_Carlist/import/BrakePad_ALFAROMEO.html"
+# second URL
+url2 = "https://www.endless-sport.co.jp/products/brake_pad/BrakePad_Carlist/import/BrakePad_LOTUS.html"
+
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58'
 }
 
-# Create a set to store processed URLs to avoid duplicates
-processed_urls = set()
+fields = ['Make','Model','Year','PartNumber']
+filename = 'Example.csv'
+data = []
 
-# Create a list to store category product names
-categoryProduct = []
+# function to scrape data from a table and append to the data list
+def scrape_data(make, model, rows):
+    for row in rows:
+        if row.has_attr('style') and 'height: 149px' in row['style'] and 'px' in row['style']:
+            continue
+        year = row.find_all('td')
+        PartNumberFront = row.find_all('td')
+        PartNumberRear = row.find_all('td')[10:]
+        if PartNumberFront and PartNumberRear:
+            PartNumber = PartNumberFront[5].text.strip() +' '+ PartNumberRear[0].text.strip()
+        else:
+            PartNumber = ""            
+        if year  and PartNumber:
+            endless = {
+                'Make': make,
+                'Model': model,                  
+                'Year': year[3].text.strip().replace('～', ' ～ '),
+                'PartNumber': PartNumber.replace(' ', '\n'),
+            }
+            data.append(endless)
+            print('Saving', endless['Make'], endless['Model'], endless['Year'], endless['PartNumber'])                
 
-# Set the URL for brake pads category and get the page content
-pageBrakepads = baseurl + '/products/brake_pad/car_list.html'
-r = requests.get(pageBrakepads, headers=headers)
+# scrape data from the first URL
+r = requests.get(url1, headers=headers)
 soup = BeautifulSoup(r.content, 'lxml')
+iframesimport = soup.find_all('iframe')
+for iframeimport in iframesimport:
+    src = iframeimport['src']
+    iframeimport_content = requests.get(src).content
+    iframeimport_soup = BeautifulSoup(iframeimport_content, 'html.parser')
+    tablesimport = iframeimport_soup.find_all('table')
+    for tableMake in tablesimport:
+        rowsMake = tableMake.find_all('tr')[1]
+        makeimport =  rowsMake.find_all('td')[1]
+    for tablemodelimport in tablesimport:
+        rowsmodelimport = tablemodelimport.find_all('tr')[2]
+        modelimport =  rowsmodelimport.find_all('td')[1]
+    rows = []
+    for table in tablesimport:
+        rows += table.find_all('tr')[5:]
+    scrape_data(makeimport.text.strip(), modelimport.text.strip(), rows)
 
-# Find all the car names on the brake pads category page
-listCarsName = []
-brakepad_search_whole = soup.find('div', id='brakepad_search_whole')
-carList = brakepad_search_whole.find('div', id='right_menu_box')
-for linkcars in carList.find_all('a', href=True):
-    listCarsName.append(linkcars['href'])
 
-# Set the field names for the CSV file
-fields = ['Year', 'Engine cc', 'Type']
-filename = 'contoh.csv'
-
-# Create a list to store all product URLs for all car models
-productList = []
-
-# Loop through all car models and get their product URLs
-for carName in listCarsName:
-    urlcar = requests.get(carName, headers=headers)
-    soup = BeautifulSoup(urlcar.content, 'lxml')
-    carNameIcons = soup.find_all('div', id='ewig_pad_whole')
-    for itemCarName in carNameIcons:
-        for href in itemCarName.find_all('a',  href=True):
-            # Append product URL to the list only if it hasn't been processed before
-            url = href['href']
-            if url not in processed_urls:
-                productList.append(baseurl + url)
-                processed_urls.add(url)
-            time.sleep(.5)
-            
-# Loop through all product URLs and save the product data to CSV file
-with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-    writer.writeheader()
-    for pageCarlist in productList:
-        r = requests.get(pageCarlist, headers=headers)
-        soup = BeautifulSoup(r.content, 'lxml')
-        time.sleep(.1)
-        data = []
-        iframes = soup.find_all('iframe')
-        for iframe in iframes:
-            src = iframe['src']
-            iframe_content = requests.get(src).content
-            iframe_soup = BeautifulSoup(iframe_content, 'html.parser')
-            tables = iframe_soup.find_all('table')
-            for table in tables:
-                rows = table.find_all('tr')[5:]
-                for row in rows:
-                    if row.has_attr('style') and 'height: 149px' in row['style'] and 'px' in row['style']:
-                        continue
-                    year = row.find_all('td')[1].text.strip().replace('～', ' ～ ')
-                    engine = row.find_all('td')[2].text.strip()
-                    type_ = row.find_all('td')[3].text.strip()
-                    endless = {
-                        'Year': year,
-                        'Engine cc': engine,
-                        'Type': type_,
-                    }
-                    data.append(endless)
-                    print('Saving',endless['Make'], endless['Year'], endless['Engine cc'], endless['Type'], endless['PartNumber'])
-
+# scrape data from the second URL
+# scrape data from the second URL
+hreflink = "https://www.endless-sport.co.jp/products/brake_pad/BrakePad_Carlist/import/BrakePad_LOTUS.html"
+r = requests.get(hreflink, headers=headers)
+soup = BeautifulSoup(r.content, 'lxml')
+data2 = []
+iframesimport = soup.find_all('iframe')
+for iframeimport in iframesimport:
+    src = iframeimport['src']
+    iframeimport_content = requests.get(src).content
+    iframeimport_soup = BeautifulSoup(iframeimport_content, 'html.parser')
+    tablesimport = iframeimport_soup.find_all('table')
+    for tableMake in tablesimport:
+        rowsMake = tableMake.find_all('tr')[1]
+        makeimport =  rowsMake.find_all('td')[1]
+        for tablemodelimport in tablesimport:
+            rowsModelimport = tablemodelimport.find_all('tr')
+            for rowModelimport in rowsModelimport:
+                colsModelimport = rowModelimport.find_all('td')
+                colsModelimport = [ele.text.strip() for ele in colsModelimport]
+                data2.append([makeimport.text.strip(), colsModelimport[0], colsModelimport[1], colsModelimport[2]])
+print(data2)
